492c077fff2ae66a557bec8299676ea2fe5d74d6
uison yoon <uison.yoon@gmail.com>
2018-08-05 00:16:28 +0900

    Add base16-shell.

--- tools.sh
+++ tools.sh
@@ -18,3 +18,20 @@ sh autogen.sh
 ./configure && make || exit -1
 sudo make install
 popd
+
+# BASE16-shell
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+
+rcfiles=( ~/.zshrc ~/.bashrc )
+for f in ${rcfiles[@]} ; do
+  if [ -f "$f" ] && ! grep -q BASE16 "$f" ; then
+echo $f
+cat << EOF >> $f
+# Base16 Shell
+BASE16_SHELL="\$HOME/.config/base16-shell/"
+[ -n "\$PS1" ] && \\
+  [ -s "\$BASE16_SHELL/profile_helper.sh" ] && \\
+  eval "\$("\$BASE16_SHELL/profile_helper.sh")"
+EOF
+  fi
+done
